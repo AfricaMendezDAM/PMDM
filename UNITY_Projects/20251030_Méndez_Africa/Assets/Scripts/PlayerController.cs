b9783e7dd57f5a3d2f783a8020223052ac24213a
IESPIRINGALLA\menpermar <amendezperera@gmail.com>
2025-11-04 21:57:52 +0100

    .

---
+++ UNITY_Projects/20251030_Méndez_Africa/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 7f;
+
+    private InputSystem_Actions controls;
+    private Rigidbody2D rb;
+    private SpriteRenderer renderer;
+    private Vector2 moveInput;
+    private bool jumpPressed;
+    private bool isGrounded = true;
+
+    void Awake()
+    {
+        controls = new InputSystem_Actions();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        //moveInput = new Vector2(rb.linearVelocity.x * speed, rb.linearVelocity.y);
+        moveInput = controls.Player.Move.ReadValue<Vector2>(); 
+        jumpPressed = controls.Player.Jump.triggered;
+
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+
+        controls.Player.Move.ReadValue<Vector2>(); 
+
+        if (moveInput.x < 0)
+        {
+            renderer.flipX = true;
+            
+        }else if(moveInput.x > 0)
+        {
+            renderer.flipX = false;
+        }
+    }
+    
+    void Jump()
+    {
+
+        if (jumpPressed && isGrounded)
+        {
+            rb.AddForce(moveInput * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+            jumpPressed = false;
+        }
+
+        isGrounded = true;
+    }
+}
