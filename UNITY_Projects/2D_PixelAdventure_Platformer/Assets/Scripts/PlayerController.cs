b6da17a8234039eaae973024e6f9cfac0a5ae6f2
PW0DBNP9\Africa <amendezperera@gmail.com>
2025-12-15 05:39:26 +0100

    Pendiente: -Añadir plataformas -Añadir prefabs y trampas -Añadir lógica counters -Añadir script menu opciones -Configurar escena Start -Configurar escena End -Mirar paso de escenas y tope si sale de cámara -Confirgurar seguimiento de la cámara -Estudiar movimiento con transform(nuevo proyecto coches) -Animaciones -Tag para pegarse a las paredes -Añadir Audio

--- UNITY_Projects/2D_PixelAdventure_Platformer/Assets/Scripts/PlayerController.cs
+++ UNITY_Projects/2D_PixelAdventure_Platformer/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System;
 using Unity.VisualScripting.FullSerializer;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -9,39 +10,70 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Vector2 moveInput;
-    private PlayerControls playerControls;
+    private PlayerControls controls;
     private Boolean jumpPressed = false;
     private Boolean isGrounded = false;
 
     void OnEnable()
     {
-        playerControls.Enable();
+        controls.Enable();
     }
 
     void OnDisable()
     {
-        playerControls.Disable();
+        controls.Disable();
     }
 
     void Awake()
     {
-        playerControls = new PlayerControls();
+        controls = new PlayerControls();
     }
     
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D> ();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-
+        moveInput = controls.Player.Move.ReadValue<Vector2>();
+        jumpPressed = controls.Player.Jump.triggered;
     }
 
     void FixedUpdate()
     {
-
+        Move();
+        Jump();
     }
 
-    
+    void Move()
+    {
+        rb.linearVelocity=new Vector2(moveInput.x*speed, rb.linearVelocityY);
+
+        if(moveInput.x<0){
+            sr.flipX=true;
+        } else if(moveInput.x>0){
+            sr.flipX=false;
+        }
+    }
+
+    void Jump()
+    {
+        if (jumpPressed && isGrounded)
+        {
+            rb.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+            isGrounded=false; 
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded=true;
+        }
+    }
+
+
 }
