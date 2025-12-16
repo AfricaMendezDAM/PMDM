1461f353d43ba6b08b83eee5a533f369df0e3115
IESPIRINGALLA\menpermar <amendezperera@gmail.com>
2025-12-16 21:56:42 +0100

    no message

--- UD2/pmdm_ud3_test_compose/app/src/main/java/es/murallaromana/pmdm_ud3_test_compose/ui/login/LoginLayout.kt
+++ UD2/pmdm_ud3_test_compose/app/src/main/java/es/murallaromana/pmdm_ud3_test_compose/ui/login/LoginLayout.kt
@@ -1,5 +1,6 @@
 package es.murallaromana.pmdm_ud3_test_compose.ui.login
 
+import android.R.attr.onClick
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.snapping.SnapPosition
@@ -12,28 +13,39 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
+import androidx.compose.ui.text.style.TextAlign.Companion.Left
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lint.kotlin.metadata.Visibility
 import es.murallaromana.pmdm_ud3_test_compose.R
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun LoginAppearance(
-    email: String = "", //TODO: No debería poner parámetros por defecto para credenciales
-    pwd: String = "",
     welcomeSms: String = "Bienvenido"
 ) {
+    var email by remember { mutableStateOf("") }
+    var pwd by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -60,17 +72,51 @@ fun LoginAppearance(
             fontSize = 24.sp
         )
 
+        Text(
+            text = "Inicia sesión para continuar",
+            fontSize = 16.sp
+        )
+
         Spacer(
             Modifier
                 .size(20.dp)
         )
 
-        TextField(
+        OutlinedTextField(
             value = email,
-            onValueChange = {emailTxt -> email = emailTxt},
-            
+            onValueChange={},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            label = { Text("Correo Electrónico") },
+            placeholder = {Text("example@email.com")}
 
         )
+
+        OutlinedTextField(
+            value = pwd,
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            label = {Text("Contraseña")},
+            placeholder = {Text("******")},
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id=R.drawable.baseline_remove_red_eye_24),
+                    contentDescription="Icono pwd ojo abierto" //TODO: Estudiar muy bien dferencia nombre parámetros entre image e icon
+                )
+            }
+        )
+
+        Button(
+            onClick = { } ,
+            contentColor = Color.Green
+        ){
+            Text(text= "¿Olvidaste tu contraseña?")
+        }
+
+
     }
 }
 
