667415967e14207ee7e7827532db95a1ee2f3a15
IESPIRINGALLA\menpermar <amendezperera@gmail.com>
2025-12-19 19:13:34 +0100

    ejercicios ejemplo

--- UD2/pmdm_ud3_test_compose/app/src/main/java/es/murallaromana/pmdm_ud3_test_compose/ejemplos_clase/ejemplosKotlin.kt
+++ UD2/pmdm_ud3_test_compose/app/src/main/java/es/murallaromana/pmdm_ud3_test_compose/ejemplos_clase/ejemplosKotlin.kt
@@ -5,12 +5,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 
-fun  saludar(param1: Float, param2: Float): Int{
-    return
+fun  saludar (nombre: String, edad: Int, miFuncion: (param1: Float, param2: Float) ->Int): Int{
+    return 1
 }
 
 fun main(){
     saludar("pepe", 20, ::sumaEntero) //funciones como parametro
     saludar("pepe", 20, ()->Unit) //formas de esribirlos
-    saludar("pepe", 20, { x: Float, y: Float)-> x.toInt()+y.toInt()})
+    saludar("pepe", 20, { x: Float, y: Float)-> x.toInt()+y.toInt()} as (Float, Float) -> Int)
 }
